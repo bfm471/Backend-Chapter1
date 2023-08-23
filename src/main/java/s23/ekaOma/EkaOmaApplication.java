07aa8db8f0d5306846901de235276f84a6aa9dff
bfm471 <bfm471@myy.haaga-helia.fi>
2023-08-23 22:16:50 +0300

    eka ajo

---
+++ src/main/java/s23/ekaOma/EkaOmaApplication.java
@@ -0,0 +1,13 @@
+package s23.ekaOma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaOmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkaOmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s23/ekaOma/EkaOmaApplicationTests.java
@@ -0,0 +1,13 @@
+package s23.ekaOma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkaOmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
